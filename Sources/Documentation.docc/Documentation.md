0648f092dbaf323cdb201688e00fc78d04062243
Mikhail Kulichkov <mikhail.kulichkov@excelsior-solutions.de>
2022-06-15 12:33:56 +0200

    init commit

---
+++ Sources/Documentation.docc/Documentation.md
@@ -0,0 +1,14 @@
+# ````
+
+Test documentation for GitHub pages
+
+## Overview
+
+Overview for test documentation for GitHub pages 
+
+## Topics
+
+### Test documentation group
+
+- ``Test Symbol 1``
+- ``Test Symbol 2``
---
+++ Sources/Documentation.docc/Resources/Code/TestCode.swift
@@ -0,0 +1,21 @@
+//
+//  TestCode.swift
+//  
+//
+//  Created by Mikhail Kulichkov on 15.06.22.
+//
+
+import SwiftUI
+
+struct CustomizedSlothView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomizedSlothView()
+    }
+}
